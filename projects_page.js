001e85693711e93693c79f4ef05c7ece0c4bc5d1
uoethan <eh20268@essex.ac.uk>
2023-04-19 14:18:18 +0100

    Projects now display in 3 columns on desktop and 1 column on mobile

--- projects_page.js
+++ projects_page.js
@@ -1,15 +1,18 @@
 $(function() {
 
     let list = $(".list");
-    list.append("<ul class = \"list-wrap\"");
+    list.append("<div class='row'>");
     jQuery.get('sketches.txt', function (data) {
 
         let lines = data.split("\n");
 
         $.each(lines, function (n, elem) {
+            if(n%3===0){
+                list.append("</div>\n<p><br></p>\n<div class='row'>")
+            }
             const splitElem = elem.split("|");
-            list.append("<a href='player.html?project="+splitElem[0]+"' class='project-link'><li class=\"list-inner\">"+splitElem[2]+"<br><img src='/Images/"+splitElem[0]+".png' class='list-image' alt='Project image'><br>"+splitElem[1]+"</li></a>");
+            list.append("<a href='player.html?project="+splitElem[0]+"' class='project-link'><div class='column'><b>"+splitElem[2]+"</b><br><img src='/Images/"+splitElem[0]+".png' class='list-image' alt='Project image'><br>"+splitElem[1]+"</div></a>");
         });
     });
-    list.append("</ul>");
+    list.append("</tr>");
 });
